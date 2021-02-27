ad9614186c951c285cca09d31adda8dd4cd7d4a8
Natalie Volk <natalievolk@rogers.com>
2021-02-27 12:48:07 -0500

    get_weight and get_terms functions for pt. 1

--- autocomplete.c
+++ autocomplete.c
@@ -1,16 +1,85 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #include "autocomplete.h"
 
-void read_in_terms(struct term **terms, int *pnterms, char *filename) {
-    char line[200];
-    FILE *fp = fopen(filename, "r");
-    for(int i = 0; i < 10; i++){
-        fgets(line, sizeof(line), fp);  //read in at most sizeof(line) characters
-                                        //(including '\0') into line.
+int get_weight(char *line) {
+    while(*line == ' ') {
+        line++;
+        //printf("%s", "hi");
+    }
+
+    // get weight
+    int weight = 0;
+    while(isdigit(*line)) {
+        weight = weight*10;
+        weight = weight + (*line-'0');
+        line++;
+    }
+    return weight;
+}
+
+char *get_term(char *line) {
+    // moves through initial whitespace
+    while(isspace(*line)) {
+        line++;
+    }
+    // moves through weight number
+    while(isdigit(*line)) {
+        line++;
+    }
+    // moves through whitespace between weight and term
+    while(isspace(*line)) {
+        line++;
     }
     
-    printf("%d\n", 100);
+    char *term = line;
+    // removes newline character from end
+    while(*line != '\0') {
+        if (*line == '\n') {
+            *line = '\0';
+        }
+        line++;
+    }
+    // returns the term
+    return term;
+}
+
+void read_in_terms(struct term **terms, int *pnterms, char *filename) {
+    char line[200];     // assuming terms not longer than 200
+    FILE *fp = fopen(filename, "r");
+
+    *pnterms = atol(fgets(line, sizeof(line), fp)); // store number of terms
+    printf("%d\n", *pnterms);
+
+    *terms = (struct term*)malloc(sizeof(struct term)*(*pnterms));
+
+    for(int i = 0; i < 2; i++){
+        fgets(line, sizeof(line), fp);  //read in at most sizeof(line) characters
+                                        //(including '\0') into line.
+        
+        int weight = get_weight(line);
+        char *term = get_term(line);
+        printf("%d %s\n", weight, term);
+
+        /*char start = *line;
+        printf("%s\n", start);
+        */
+        /*
+        while(*line == ' ') {
+            (*line)++;
+            printf("%s", "hi");
+        }
+        printf("\n");
+        */
+        
+        
+
+        //printf("%ld\n", &line);
+        //printf("%c\n", line[0]);
+        // *(*terms + sizeof(struct term)*i) = 
+    }    
 }
 
 int lowest_match(struct term *terms, int nterms, char *substr) {
--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main(void)
     struct term *terms;
     int nterms;
     read_in_terms(&terms, &nterms, "cities.txt");
+    /*
     lowest_match(terms, nterms, "Tor");
     highest_match(terms, nterms, "Tor");
     
@@ -13,4 +14,5 @@ int main(void)
     autocomplete(&answer, &n_answer, terms, nterms, "Tor");
     //free allocated blocks here -- not required for the project, but good practice
     return 0;
+    */
 }
