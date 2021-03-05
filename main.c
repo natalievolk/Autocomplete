ede0474ac8b3e266656701cdcb6f09af18bc4897
Natalie Volk <natalievolk@rogers.com>
2021-03-04 21:31:12 -0500

    final :)

--- main.c
+++ main.c
@@ -6,15 +6,15 @@ int main(void)
 {
     struct term *terms;
     int nterms;
-    read_in_terms(&terms, &nterms, "wiktionary.txt");
+    read_in_terms(&terms, &nterms, "cities.txt");
     
-    printf("lowest match %d\n", lowest_match(terms, nterms, "that"));
+    printf("lowest match %d\n", lowest_match(terms, nterms, "Tor"));
 
-    printf("highest match %d\n", highest_match(terms, nterms, "that"));
+    printf("highest match %d\n", highest_match(terms, nterms, "Tor"));
     
     struct term *answer;
     int n_answer;
-    autocomplete(&answer, &n_answer, terms, nterms, "that");
+    autocomplete(&answer, &n_answer, terms, nterms, "Tor");
 
     printf("%d\n", n_answer);
 
